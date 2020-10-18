322343576239aad3001c3620072cc4f21d2c50b3
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-18 22:31:52 +0530

    routing setup login by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'feedback', loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule) }, { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }];
+const routes: Routes = [
+{ path: 'feedback', loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule) },
+{ path: 'products', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
+{ path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
