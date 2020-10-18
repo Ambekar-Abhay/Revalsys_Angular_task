4768ef72204be205604b1388c9d7bf1965b53a79
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-19 01:27:39 +0530

    reactive form added for feedback

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
+  feedbackForm:FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.feedbackForm=new FormGroup({
+      name:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required),
+      message:new FormControl('asss',Validators.required),
+
+    })
   }
 
 }
