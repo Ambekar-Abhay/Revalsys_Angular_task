46d901f8b556eca8d6ff48d6a0887fce32b4a1dd
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-18 22:14:35 +0530

    generated feature  module login,product,feedback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'feedback', loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule) }, { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
