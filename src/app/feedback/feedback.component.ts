d546d48da305063566fd47a1077a397231673d87
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-20 20:29:59 +0530

    storing feedback form data in  session storage

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -8,16 +8,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class FeedbackComponent implements OnInit {
   feedbackForm:FormGroup
+  isSuccess: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
     this.feedbackForm=new FormGroup({
       name:new FormControl('',Validators.required),
-      email:new FormControl('',Validators.required),
-      message:new FormControl('asss',Validators.required),
+      email:new FormControl('',[Validators.required,Validators.pattern(/^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]),
+      message:new FormControl('',Validators.required),
 
     })
   }
-
+submitFeedback(){
+  const data=JSON.stringify(this.feedbackForm.value)
+  sessionStorage.setItem('feedbackDetails',data)
+  this.feedbackForm.reset()
+  this.isSuccess=true
+  setTimeout(() => {
+    this.isSuccess=false
+  }, 2000);
+}
 }
