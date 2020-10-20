e38462c0c71e9a14828f66afde1110dedc89d6bf
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-21 00:50:24 +0530

    product data binding in progress

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -8,7 +8,20 @@ import { ApiService } from '../services/api.service';
 })
 export class ProductComponent implements OnInit {
   productDetails: any;
-
+  filter=[{"name":'hightolow',"id":1},{"name":'lowtohigh',"id":2}]
+  productlength: number=12;
+  @HostListener('window:scroll', ['$event']) 
+  scrollHandler(event) {
+    if(this.productlength==12){
+      this.productlength=12*2
+    }
+    if(this.productlength==24){
+      this.productlength=this.productDetails.length
+    }
+    if(this.productlength==this.productDetails.length){
+      this.productlength=this.productDetails.length
+    }
+  }
   constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
@@ -19,8 +32,17 @@ export class ProductComponent implements OnInit {
       if(resp){
         this.productDetails=resp
         console.log(resp)
+        this.productDetails.map(x=>{
+          var index = x.Images.indexOf("|");
+         var img= x.Images.slice(0,index);
+          x.Images=x.ListImagePath+img
+        })
       }
     })
   }
+  // scrollHandler(event){
+  //   alert("welcometo scroll event")
+
+  // }
 
 }
