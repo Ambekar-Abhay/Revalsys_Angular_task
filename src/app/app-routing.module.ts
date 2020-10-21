1f3c8fe5c36dc11790e53df94c5a18ccad9c9254
Abhay ambekar <abhayambekar03@gmail.com>
2020-10-21 18:39:19 +0530

    AuthGuard implementation to secure route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   {
     path:'dashboard',
-    component:NavbarComponent,children:[
+    component:NavbarComponent,canActivate:[AuthGuard], children:[
       { path: 'feedback', loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule) },
       { path: 'products', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
     ]
